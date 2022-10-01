de7a3922ce65c96daa7135c8637fc7409716c33a
Pratik Rai <pratikkumar399@gmail.com>
2022-10-01 22:52:25 +0530

    Time: 6 ms (100.00%), Space: 7.5 MB (100.00%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
   
-  set<int> st ;
    bool equalFrequency(string word) {
-     unordered_map<char,int> mp ;
-      for(int i = 0 ; i< word.size() ; i++){
-        mp[word[i]]++ ;
-      }
-      for(int i=0; i<word.size(); i++){
-        mp[word[i]]-- ;
-        int freq = (i<word.size()-1) ? mp[word[i+1]]: mp[word[i-1]];
-        if(valid(mp , freq)) return true ;
-        mp[word[i]]++ ;
-      }
-      return false ;
+      for(int i=0;i<word.size();i++) //this is for selecting element we want to delete
+        {
+            map<char,int>mp;
+            map<int,int>freq;
+            for(int j=0;j<word.size();j++)
+            {
+                if(j!=i)  //means we have not taken ith element
+                    mp[word[j]]++;
+            }
+            
+            for(auto it:mp)
+                freq[it.second]++;
+            
+            if(freq.size()==1)  
+                return true;
+            
+           
+         }
+        
+        return false;
     }
   
-  bool valid(unordered_map<char,int> mp , int freq){
-          for(auto it : mp){
-            if(it.second>= 1 && it.second!= freq){
-              return false;
-            }
-          }
-    
-          
-           return true ;
-        }
+ 
 };
 
