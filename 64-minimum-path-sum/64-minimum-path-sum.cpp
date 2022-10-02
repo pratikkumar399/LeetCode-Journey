b3e402e2b811a1050851bff57818b5e2d0692586
Pratik Rai <pratikkumar399@gmail.com>
2022-10-02 19:26:26 +0530

    Time: 3 ms (99.80%), Space: 10.1 MB (56.66%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -2,24 +2,7 @@ class Solution {
 public:
     
     
-    int tabulation(vector<vector<int>>& grid , int n , int m , vector<vector<int>> dp){
-      dp[n-1][m-1] = grid[n-1][m-1] ;
-      
-      for(int i = m-2 ; i >=0 ; i-- ){
-        dp[n-1][i]= dp[n-1][i+1] + grid[n-1][i] ;
-      }
-      for(int i = n-2; i >=0 ; i-- ){
-        dp[i][m-1]= dp[i+1][m-1] + grid[i][m-1] ;
-      }
-      for(int i = n-2 ; i >=0 ; i-- ){
-        for(int j = m-2 ; j >=0 ; j-- ){
-              dp[i][j] = min(dp[i+1][j] , dp[i][j+1]) + grid[i][j] ;
-         }
-      }
-      
-      return dp[0][0] ;
-      
-    }
+    
   
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size() ;
