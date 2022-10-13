36f21e976ad866be7a0f97a9058c984c6eb9dcbe
Pratik Rai <pratikkumar399@gmail.com>
2022-10-13 21:54:06 +0530

    Time: 13 ms (72.56%), Space: 7.4 MB (55.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,14 +4,18 @@ public:
       if(str.size() !=  t.size() ) return false ;
       vector<int> frequency(26 , 0 ) ;
       
+      
+      // storing frequency of each element 
       for(auto it : str){
         frequency[it-'a'] = frequency[it- 'a'] + 1 ;
       }
       
+      // decrementing the frequency of each element in another string 
       for(auto it : t){
         frequency[it-'a'] = frequency[it- 'a'] - 1 ;
       }
       
+      // if the strings are the anagrams of each other than they will return 0 otherwise they are not anagrams of each other 
       for(int i = 0 ; i < 26 ; i++ ){
       if(frequency[i] != 0) return false ;
       }
