66318b2226cbec1b23a0de281ce7136ae2c9b5ce
Pratik Rai <pratikkumar399@gmail.com>
2022-10-13 18:09:31 +0530

    Time: 15 ms (65.97%), Space: 14 MB (79.22%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -4,11 +4,8 @@ public:
   void dfs(int i , int j , int inColor ,int newColor ,vector<vector<int>> &image){
     int n = image.size() ;
     int m = image[0].size() ;
-      if( i < 0 || j < 0) return ;
-      if( i >= n || j >= m) return ;
-      if(image[i][j] != inColor)  return ;
+      if( i < 0 || j < 0 || i >=  n || j>= m || image[i][j] != inColor) return ;
        
-    
       image[i][j] = newColor ;
       dfs(i-1,j,inColor,newColor,image ) ;
       dfs(i+1,j,inColor,newColor,image ) ;
