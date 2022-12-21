87ca286d6777a6a5b06baa9b74e8c41cc94d542a
Pratik Rai <pratikkumar399@gmail.com>
2022-12-21 14:50:45 +0530

    Time: 3 ms (86.63%), Space: 7.6 MB (5.10%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,38 +1,11 @@
 class Solution {
 public:
     int myAtoi(string str) {
-        int sign = 1 ,  base = 0 , i = 0 ; 
-        // what we have to do is that we have to ignore the whitespaces 
-      // and also take casre that if there is a negative sign then the number remains negative at last 
-      // so the approach can be that if there is any whitespace in the beginning simply ignore the whitespaces 
-      // and also if there is a negiatuve sign then simply store the sign in a veriable and then simply what we can do is that at last multiply the number with that sign 
-      // this will give us the requred answer 
-      while(str[i] == ' '){
-        // ignoring the whitespaces 
-        i++ ;
-      }
+      int res = 0; 
+        stringstream ss ;
+      ss<<str ;
+      ss>>res;
       
-       if (str[i] == '-' || str[i] == '+') 
-     {
-        sign = 1 - 2 * (str[i++] == '-');
-      }
-      
-      while(str[i] >= '0' && str[i] <= '9' ){
-        if (base > INT_MAX / 10
-            || (base == INT_MAX / 10 
-            && str[i] - '0' > 7)) 
-        {
-            if (sign == 1)
-                return INT_MAX;
-            else
-                return INT_MIN;
-        }
-        base = 10 * base + (str[i++] - '0');
+      return res ;
     }
-    return base * sign;
-      }
-      
-      
-      
-    
 };
