3c6ba37029a70c04490ff53e51eba4d9f32371a8
Pratik Rai <pratikkumar399@gmail.com>
2022-12-22 00:37:16 +0530

    Time: 114 ms (5.11%), Space: 26.3 MB (6.76%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
       k %=nums.size();
-      // cout<< k << endl ;  
-      //   reverse(nums.begin(), nums.end()); //7 6 5 4 3 2 1
-      //   reverse(nums.begin(), nums.begin()+k);  //5 6 7 
-      //   reverse(nums.begin()+k, nums.end()); //1 2 3 4 5
-        vector<int>  ans ;
-        for(auto it :  nums){
-          ans.push_back(it) ; 
-        }
-        nums.clear() ;
-      // reverse(ans.begin() , ans.end()) ; // 7 6 5 4 3 2 1 || 1 2 3 4 5 6 7
-        for(int i =ans.size() -1  ; i >=ans.size() - k ;i--){ // 6  5 4 || 4 = 7-3
-          cout<<ans[i]<<" " ;
-          nums.push_back(ans[i]); // nums -> 7 6 5 
-          
-        }
-      
-      reverse(nums.begin() , nums.end()) ; // 5 6 7
-      for(int i = 0 ; i < ans.size()-k ; i++){
-        nums.push_back(ans[i]) ;
+      // 7 6 5 4 3 2 1 
+      int n = nums.size() ;
+     reverse(nums.begin() , nums.end()) ;
+      nums.clear() ;
+      vector<int> ans ;
+      // 5 6 7 
+      for(int i = k - 1 ; i >= 0 ; i--){
+        ans.push_back(nums[i]) ;
       }
       
+      for(int i = n - 1  ; i >= k  ; i-- ){
+        ans.push_back(nums[i]) ;
+      }
+      
+      for(auto it :  ans){
+        nums.push_back(it) ;
+      }
       ans.clear() ;
     } 
 };
