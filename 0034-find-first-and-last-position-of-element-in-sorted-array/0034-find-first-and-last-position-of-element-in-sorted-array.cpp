716267eab0198a954644cada8036dddad0fff28d
Pratik Rai <pratikkumar399@gmail.com>
2022-12-26 12:44:00 +0530

    Time: 10 ms (79.72%), Space: 13.6 MB (85.91%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     
-    int binarysearch(vector<int> &nums ,int target){
+  // since we are finding the first occurence of a digit so we search in the left side 
+  // even if the target is found continue searching the left side to get the correct answer 
+    int binarysearch(vector<int> &nums ,int target , bool ans){
         int start = 0 ;
         int end = nums.size() - 1 ;
         int result = -1 ;
@@ -11,7 +13,12 @@ public:
             
             if(nums[mid] == target){
                 result = mid ;
-                end = mid - 1 ;
+                if(ans == true){
+                  end = mid - 1 ;
+                }
+              else {
+                start = mid + 1;
+              }
                
                 
             }
@@ -25,35 +32,11 @@ public:
         return result ;
     } 
     
-    
-     int binarysearch2(vector<int> &nums ,int target){
-        int start = 0 ;
-        int end = nums.size() - 1 ;
-        int result = -1 ;
-        
-        while(start <= end){
-            int mid = start + (end -start) /2 ;
-            
-            if(nums[mid] == target){
-                result = mid ;
-               
-                start = mid + 1 ;
-                
-            }
-            else if(target  < nums[mid]){
-                end = mid - 1 ;
-            }
-            else{
-                start = mid + 1 ; 
-            }
-        }
-         return result ;
-    } 
-    
     vector<int> searchRange(vector<int>& nums, int target) {
         
-        int first = binarysearch(nums,target) ;
-        int last = binarysearch2(nums,target) ;
+        bool ans =  false;
+        int first = binarysearch(nums,target , true) ;
+        int last = binarysearch(nums,target ,  false ) ;
         
         vector<int> out ;
         out.push_back(first) ;
