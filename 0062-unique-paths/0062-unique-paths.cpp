986bb35b64b71204c17e81683ae91318d4d479a4
Pratik Rai <pratikkumar399@gmail.com>
2022-12-29 23:30:24 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (30.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -19,14 +19,13 @@ public:
   
   
     int uniquePaths(int m, int n) {
-             int dp[m][n] ;
+             vector<vector<int>> dp(m , vector<int> (n)) ;
              // dp[0][0] = 1 ; 
       int left , up ;
             for(int i = 0 ; i < m  ; i++){
               for(int j = 0 ; j < n ; j++){
                 if(i == 0 && j == 0) dp[i][j] =  1 ;
-                else{
-                  up = 0 , left = 0 ; 
+                else{up = 0 , left = 0 ;
                  if(i> 0 ) up = dp[i-1][j] ;
                   if(j > 0 )  left = dp[i][j-1] ;
                   
