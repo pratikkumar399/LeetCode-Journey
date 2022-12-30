abf2ffd41d4b7fba3a155d3f88c5f1844a778862
Pratik Rai <pratikkumar399@gmail.com>
2022-12-30 20:05:48 +0530

    Time: 12 ms (76.23%), Space: 10.5 MB (6.81%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -6,7 +6,7 @@ public:
         int m = grid[0].size() ;
         
         vector<vector<int>> dp(n ,vector<int> (m , 0)) ;
-        vector<int> prev(n , 0) ;
+        vector<int> prev(m , 0) ;
         for(int i = 0 ; i < n ; i++){
            vector<int> cur(m , 0) ;
           for(int j = 0 ; j < m ; j++){
