b67225a22367e076f2c4f44d01f06dc9883e670b
Pratik Rai <pratikkumar399@gmail.com>
2023-01-01 15:34:47 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (48.73%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -8,9 +8,9 @@ public:
     cout<<matrix[i][j] <<endl ;
       if(dp[i][j] != -1 ) return dp[i][j] ;
       int left = minipath(dp , matrix , i - 1 , j) ;
-      int right =  minipath(dp , matrix , i , j -1) ;
+      int up =  minipath(dp , matrix , i , j -1) ;
       
-      return dp[i][j] = left + right ;
+      return dp[i][j] = left + up ;
       
     }
   
@@ -18,11 +18,31 @@ public:
   
   
     int uniquePathsWithObstacles(vector<vector<int>>& matrix) {
+      if(matrix[0][0]==1) return 0;
       int n = matrix.size() ;
       int m = matrix[0].size() ;
-        vector<vector<int>>  dp(n ,  vector<int> (m , -1)) ;
+        // vector<vector<int>>  dp(n ,  vector<int> (m , -1)) ;
+      vector<vector<int>>  dp(n , vector<int> (m )) ;
+      dp[0][0] = 1 ;
+      for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++){
+          if(i != 0 && j != 0 )  dp[i][j] = 0 ;
+         
+          if ( matrix[i][j] != 1) 
+          { 
+            if(i > 0) dp[i][j] += dp[i-1][j] ;
+            if(j > 0)dp[i][j] += dp[i][j-1] ;
+          }
+          
+          
+          
+          
+        }
+      }
       
-     return minipath(dp ,matrix , n-1 , m-1 ) ;
+      
+      
+     return dp[n- 1][m-1] ;
     }
 };
 
