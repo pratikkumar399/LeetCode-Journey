5db45b96f97d3f2fe2ce9b0516e3875711c797de
Pratik Rai <pratikkumar399@gmail.com>
2023-01-02 16:17:57 +0530

    Time: 3 ms (98.59%), Space: 8.7 MB (64.82%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,19 +4,21 @@ public:
  
     int minimumTotal(vector<vector<int>>& triangle) {
           int n = triangle.size() ;
-      vector<vector<int>>  dp (n  , vector<int> (n , 0)) ;
+      // vector<vector<int>>  dp (n  , vector<int> (n , 0)) ;
+      vector<int>  front(n ,  0) ,  cur(n , 0) ;
       
       
-      // initially taking up all the values from the first col
-      for(int i = 0 ; i < n ;i++) dp[n-1][i] = triangle[n-1][i] ;
+      // initially taking up all the last rows value for the dp matrix
+      for(int i = 0 ; i < n ;i++) front[i] = triangle[n-1][i] ;
       for(int i = n-2 ;  i>= 0 ; i--){
         for(int j = i ; j >= 0 ; j--){
-          int left = triangle[i][j] + dp[i+1][j] ;
-          int right = triangle[i][j] + dp[i+1][j+1] ;
-          dp[i][j] = min(left , right) ;
+          int left = triangle[i][j] + front[j] ;
+          int right = triangle[i][j] + front[j+1] ;
+          cur[j] = min(left , right) ;
         }
+        front = cur ;
       }
-          return dp[0][0] ;
+          return front[0] ;
     }
 };
 
