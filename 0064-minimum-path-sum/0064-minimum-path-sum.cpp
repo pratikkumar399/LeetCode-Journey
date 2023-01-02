b54e4dd853ab06fab6d84c8434b8456c942df794
Pratik Rai <pratikkumar399@gmail.com>
2023-01-02 15:48:33 +0530

    Time: 10 ms (84.40%), Space: 10.2 MB (32.38%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
       
+  
+    int minipath(vector<vector<int>> &dp , vector<vector<int>> &grid ,  int i , int j){
+      if(i== 0 && j == 0) return 1 ;
+      if(i  < 0 || j < 0 ) return 1e9 ;
+      
+      
+      if(dp[i][j] != -1 ) return dp[i][j] ;
+      int left =  grid[i][j] + minipath(dp , grid , i ,  j - 1) ;
+      int up = grid[i][j] + minipath(dp , grid ,  i - 1 , j ) ;   
+      
+      
+      return dp[i][j] = min(left , up) ;
+    }
+  
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size() ;
         int m = grid[0].size() ;
         
         vector<vector<int>> dp(n ,vector<int> (m , 0)) ;
-        vector<int> prev(m , 0) ;
+      
         for(int i = 0 ; i < n ; i++){
-           vector<int> cur(m , 0) ;
           for(int j = 0 ; j < m ; j++){
-           
-            if(i== 0 && j == 0) cur[j] = grid[0][0] ;
+            // the base case 
+            if(i== 0 && j == 0) dp[i][j] = grid[0][0] ;
             else{
-                int up = grid[i][j] ;
-                if(i> 0 ) up +=  prev[j] ;
-                else up+= 1e9 ;
-              
-              
-                int left = grid[i][j] ;
-                if(j>0) left +=  cur[j-1] ;
-                else left += 1e9 ;
-              
-              
-                cur[j] = min(left , up ); 
+              int up = grid[i][j] ;
+              if(i> 0 ) up +=  dp[i-1][j] ;
+              else up+= 1e9 ;
+              int left = grid[i][j] ;
+              if(j>0) left +=  dp[i][j-1] ;
+              else left += 1e9 ;
+               dp[i][j] = min(left , up ); 
             }
-          }
-          prev = cur ;
+             }
         }
         
-        return prev[m-1];
+        return dp[n-1][m-1];
     }
 };
 // cost is the minimum path of the current paths alongwith the sum of previous paths
