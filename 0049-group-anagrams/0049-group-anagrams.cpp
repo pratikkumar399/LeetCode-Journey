d9a512aec4c0e06830e668dc6fa3fa81b20b27c7
Pratik Rai <pratikkumar399@gmail.com>
2023-01-03 15:34:32 +0530

    Time: 70 ms (49.54%), Space: 20.7 MB (58.71%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans ;
+        unordered_map<string ,vector<string>> um ;
+        for(int i = 0 ; i<strs.size() ;i++) {
+           string str = strs[i] ;
+           sort(strs[i].begin() , strs[i].end()) ;
+           um[strs[i]].push_back(str) ;
+        }
+        
+        for(auto i : um){
+          ans.push_back(i.second) ;
+        }
+      
+      return ans ;
+    }
+};
+
+// sort the words and then we can keep the similar kind of words togerther 
+// what we can do is that we can creat a map and then store all the words in that map 
+// also the map can have a vector of strings
+// the vector of strings can then have all the related words together 
+// before sorting we need to keep up the original string as it is so that we can letter on use it to store it any place 
