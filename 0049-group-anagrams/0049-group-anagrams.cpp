dcc9590ab39ec490d53c22799d5fc8de4ce5e566
Pratik Rai <pratikkumar399@gmail.com>
2023-01-03 15:37:42 +0530

    Time: 37 ms (87.84%), Space: 20.8 MB (45.59%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      
+        if(strs.size() == 1) return {{strs[0]}} ;
         vector<vector<string>> ans ;
         unordered_map<string ,vector<string>> um ;
         for(int i = 0 ; i<strs.size() ;i++) {
@@ -22,3 +24,5 @@ public:
 // also the map can have a vector of strings
 // the vector of strings can then have all the related words together 
 // before sorting we need to keep up the original string as it is so that we can letter on use it to store it any place 
+
+// this solution takes O(n log n ) time for sorting and the inserion in unordered map is a constant operation
