77dd6b6bd0673dfb999e3f4e466dbf6c13d2d01e
Pratik Rai <pratikkumar399@gmail.com>
2023-01-07 03:16:09 +0530

    Time: 8 ms (43.93%), Space: 12.2 MB (38.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,7 +2,34 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-        next_permutation(nums.begin(), nums.end());
+        // next_permutation(nums.begin(), nums.end());
+          int pivot = -1 ;
+      
+        for(int i = nums.size() - 1  ; i > 0 ; i-- ){
+          if(nums[i-1] < nums[i]){
+            pivot =  i -1 ;
+            break ;
+          }
+        }
+      
+      if(pivot < 0) {
+        reverse(nums.begin() , nums.end()) ; 
+        return ;
+      }
+      for(int i = nums.size() - 1  ; i > 0 ; i-- ){
+          if(nums[i] > nums[pivot]){
+            swap(nums[i] ,  nums[pivot]) ;
+            reverse(nums.begin() + pivot + 1 ,nums.end()) ;
+            break ;
+          }
+        }
+      
+      
+      
      
     }
 };
+
+// we have to find permutation of an array of integers 
+// basically permutation is the linear  ordering of array members 
+// 
