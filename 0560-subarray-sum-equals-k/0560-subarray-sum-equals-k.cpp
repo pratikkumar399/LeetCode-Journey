5181c38ba3de7cb55857fa5b63a1084b24e1c95e
Pratik Rai <pratikkumar399@gmail.com>
2023-01-09 17:17:25 +0530

    Time: 124 ms (61.50%), Space: 47.1 MB (19.78%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,21 +4,15 @@ public:
         int count = 0 ;
         int sum = 0 ;
         unordered_map<int,int> m ;
-        // m[sum]++ ;
+        m[sum]++ ;
        
         for(int i = 0 ; i < arr.size(); i++){
            sum  += arr[i];
-          if(sum == k ) count++ ;
-          
-          // 6 - 3 
+           // if(sum == k ) count++ ;
            int ans = sum - k;
-          //1 2 3
-          // (1  , 1) (3 , 1)  (6 , 1)  
-          
-          
-             count += m[ans];
-          m[sum]++ ;
-           }
+           count += m[ans];
+           m[sum]++ ;
+          }
         return count ;
     }
 };
