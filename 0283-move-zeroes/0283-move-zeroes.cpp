1423189105d4f25d6a6e745617e73752af780689
Pratik Rai <pratikkumar399@gmail.com>
2023-01-13 02:33:06 +0530

    Time: 27 ms (73.04%), Space: 19.3 MB (16.80%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -7,16 +7,16 @@ public:
       int j = 0 ;
       for(int i = 0 ; i < n  ; i++){
          if(list[i] != 0){
-           list[j] = list[i] ;
+           swap(list[j] ,list[i]) ;
            j++ ;
          }
       }
       
       // res.resize(n) ;
       
-      for(int i = j ; i < n ; i++){
-        list[i] = 0;
-      }
+//       for(int i = j ; i < n ; i++){
+//         list[i] = 0;
+//       }
       
     }
 };
