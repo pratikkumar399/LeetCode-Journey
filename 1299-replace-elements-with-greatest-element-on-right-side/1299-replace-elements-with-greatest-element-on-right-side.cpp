5cf534cecdce3cad068c85cfcf5dae5b78db9094
Pratik Rai <pratikkumar399@gmail.com>
2023-01-13 15:29:41 +0530

    Time: 116 ms (85.35%), Space: 68.6 MB (36.64%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -5,8 +5,14 @@ public:
       int n = arr.size() ;
       if(n == 1 ) return {-1} ;
       int maxi = -1 ;
+      
       int prev ;
       for(int i = n-1 ; i >= 0 ; i--){
+       //  fixing the n-i th element as the element
+        // prev= 1 
+        // arr[5] = 1 
+        // prev = 6 
+        // arr[4] = 6 
         prev=  arr[i] ;
         arr[i] = maxi ;
         maxi= max(prev , maxi) ;
@@ -27,4 +33,7 @@ public:
 //  the required element 
 
 // let us optimize our 2d loop approach to do our work in a better time complexity because the brute force approach gives us tle /
-// 
+// from the question it is clear that we have to only take the rightmost element into consideration 
+// now if we see from the back side then we can clearly see that the last element has -1 as its max in the right
+// the seconde last element has the last element as the max
+// so what we can do is that iterate from the back side and then take up tha max of all the elements already traversed from the back side and then replace that element witht the maximum one 
