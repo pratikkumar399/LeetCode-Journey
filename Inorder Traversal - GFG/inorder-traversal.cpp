d1bdd168fe9e673cfc106bff1ca24eb95951fce8
Pratik Rai <pratikkumar399@gmail.com>
2023-01-20 23:56:02 +0530

    Added solution - LeetHub

--- Inorder Traversal - GFG/inorder-traversal.cpp
+++ Inorder Traversal - GFG/inorder-traversal.cpp
@@ -33,20 +33,28 @@ class Solution {
   public:
     // Function to return a list containing the inorder traversal of the tree.ro
     
-    void util(Node* root , vector<int> &ans){
-        
-        if(root == NULL) return ;
-        
-        util(root->left , ans) ;
-        ans.push_back(root->data) ;
-        util(root->right , ans) ;
-    }
+   
     
     vector<int> inOrder(Node* root) {
-        // Your code here
-        vector<int> ans ;
-        util(root , ans) ; 
-        return ans ;
+       stack<Node*> st ;
+       vector<int> ans ;
+       
+       while(true){
+           if(root!= NULL){
+               st.push(root) ;
+               root= root->left ;
+           }
+           else{
+               if(st.empty()) break ;
+               root = st.top() ;
+               st.pop() ;
+               ans.push_back(root->data) ;
+               root= root->right ;
+               
+           }
+       }
+       
+       return ans ;
     }
 };
 
