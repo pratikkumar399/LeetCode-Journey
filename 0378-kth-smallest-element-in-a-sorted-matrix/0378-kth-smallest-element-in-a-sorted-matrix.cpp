ef27a00d1d6c2efd4ee4a9b237699da4c0201791
Pratik Rai <pratikkumar399@gmail.com>
2023-01-24 17:41:52 +0530

    Time: 52 ms (42.75%), Space: 15.3 MB (10.64%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -11,13 +11,10 @@ public:
       
       int count = 0 ;
       while(!pq.empty()){
-        if(k== 1) break;
+        if( k == 1) break;
         int top = pq.top();
         pq.pop() ;
-        cout<<"top-> "<<  top<<" " ;
         count++ ;
-        cout<<count <<endl ;
-        
         if(count  == k - 1) break ;
       }
       return pq.top() ;
