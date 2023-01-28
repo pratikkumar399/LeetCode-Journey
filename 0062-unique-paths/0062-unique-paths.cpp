2c0b49fc75ad452abe3d75a342479a26b58f6583
Pratik Rai <pratikkumar399@gmail.com>
2023-01-29 03:17:46 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (31.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,12 +3,16 @@ public:
   
     int unique(int i ,  int j ,  vector<vector<int>> &dp){
         
+      // defining the base ..case. 
+      //. so if .we reach the start .of the grid then it is certain that we have found a path 
       if(i == 0 && j == 0) return  1 ;
       
+      // if we exceed the boundaries then we cannot take that path 
       if(i < 0 || j < 0) return 0 ;
       
       if(dp[i][j] != -1) return dp[i][j] ;
       
+      // taking into account the total options that we have 
       int left = unique(i-1, j  , dp) ;
       int right = unique(i,j-1 , dp) ;
       
@@ -17,9 +21,12 @@ public:
   
   
   
-  
     int uniquePaths(int m, int n) {
+      
+      // tabulating the memoized recursive approach 
              vector<vector<int>> dp(m , vector<int> (n)) ;
+      
+      // since we depedn on the previus index for the next answer we can space optimize our solution 
               vector<int> prev (n , 0) ;
              // dp[0][0] = 1 ; 
       int left , up ;
