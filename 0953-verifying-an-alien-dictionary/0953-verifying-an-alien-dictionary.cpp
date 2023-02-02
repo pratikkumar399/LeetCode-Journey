9acc378cd9a28d9f6397a3ae33ec10fc2dd6296a
Pratik Rai <pratikkumar399@gmail.com>
2023-02-02 21:54:33 +0530

    Time: 6 ms (65.24%), Space: 9.5 MB (83.64%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        int mp[26]; 
+      
+       for(int i = 0 ; i < order.size() ; i++){
+         mp[order[i] - 'a'] = i ;
+       }
+      for(auto &it : words){
+          for(auto &i : it ){
+            i = mp[i - 'a'] ;
+          }
+       }
+      return is_sorted(words.begin() , words.end()) ;
+    }
+};
