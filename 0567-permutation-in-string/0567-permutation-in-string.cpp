d3aef9623575f917e58d95e84f7a8ff88a5e7151
Pratik Rai <pratikkumar399@gmail.com>
2023-02-05 03:24:16 +0530

    Time: 11 ms (58.42%), Space: 7.3 MB (45.40%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        unordered_map< char, int >mp;    //Use hashmap to store the frequencies of all the characters present in string s1.
-        for(auto it : s1){
-            mp[it]++;
+    bool checkInclusion(string p, string s) {
+        vector<int> phash(26 , 0) ;
+        vector<int> hash(26 , 0) ;
+        vector<int> ans ;   
+      
+       int p_size = p.size() ;
+       int s_size = s.size() ;
+       
+      if(s_size < p_size) return false ;
+      
+      int left = 0 , right = 0 ;
+      
+       while(right < p_size){
+         phash[p[right] - 'a']++ ;
+         hash[s[right] - 'a']++ ;
+         right++ ;
+       }
+      
+      right-- ;
+      while(right < s_size){
+        if(phash == hash){
+          return true ;
         }
-        int count = mp.size();         //Use the count variable to see if all of the characters in the map have the same frequency, indicating that an anagram has been found.
-        int i = 0, j = 0;
-        int k = s1.size();             //Window Size
-        while(j < s2.size()){
-            if(mp.find(s2[j]) != mp.end()){      //If a character is found that already exists in the map, reduce its frequency by one.
-                mp[s2[j]]--;
-                if(mp[s2[j]] == 0){     //If the frequency of a specific character on the map is 0, it means that all occurrences of that character is found inside the current window size.
-                    count--;
-                }
-            }
-            if(j-i+1 < k){
-                j++;
-            }
-            else if(j-i+1 == k){
-				if(count == 0){    //Anagram found 
-					return true;  
-				}
-                if(mp.find(s2[i]) != mp.end()){  //Check if that character is present in the map while sliding the window, then increase its frequency by one, as we decreased its frequency when we first met it while crossing the window.
-                    mp[s2[i]]++;
-                    if(mp[s2[i]] == 1){
-                        count++;
-                    }
-                }
-                i++;
-                j++;
-            }
+        right++ ;
+        if(right != s_size){
+          hash[s[right] - 'a'] += 1 ;
         }
-        return false;
+        
+        hash[s[left] - 'a'] -= 1 ;
+        
+        left++ ;
+      }
+      return false ;
     }
 };
