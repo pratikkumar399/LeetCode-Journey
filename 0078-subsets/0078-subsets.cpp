cda721105af0c671977474388159fb01ccf9e41a
Pratik Rai <pratikkumar399@gmail.com>
2023-02-04 22:20:08 +0530

    Time: 0 ms (100.00%), Space: 10.9 MB (41.76%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,44 @@
-class Solution {
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-      int n = nums.size() ;
-        vector<vector<int>>  subs(1<<n) ; 
-        
-      
-        for(int i = 0 ; i < (1 << n) ; i++){
-          
-           for(int j = 0 ; j < n ; j++){
-             if(i & (1<<j)){
-               subs[i].push_back(nums[j]) ;
-             }
-           }
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> subs;
+//         vector<int> sub;
+//         subsets(nums, 0, sub, subs);
+//         return subs;
+//     }
+// private:
+//     void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+//         subs.push_back(sub);
+//         for (int j = i; j < nums.size(); j++) {
+//             sub.push_back(nums[j]);
+//             subsets(nums, j + 1, sub, subs);
+//             sub.pop_back();
+//         }
+//     }
+// };
+
+
+class Solution
+{
+    public:
+    
+    void subsets(int index , vector<vector<int>> &ans , vector<int> &arr , vector<int> &ds){
+        if(index == arr.size()){
+            ans.push_back(ds) ;
+            return ;
         }
-      return subs ;
+        subsets(index+1,ans,arr,ds);
+        ds.push_back(arr[index]) ;
+        subsets(index+1,ans,arr,ds) ;
+        ds.pop_back();
+    }
+    vector<vector<int> > subsets(vector<int>& A)
+    {
+        //code here
+        vector<vector<int>> ans ;
+        vector<int> ds ;
+        subsets(0, ans , A , ds) ;
+        return ans;
     }
 };
+
