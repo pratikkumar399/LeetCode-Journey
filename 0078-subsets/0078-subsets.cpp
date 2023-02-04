c202182ca9181032e575886589a6f66cd621e760
Pratik Rai <pratikkumar399@gmail.com>
2023-02-04 22:24:38 +0530

    Time: 3 ms (65.89%), Space: 15.8 MB (35.62%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,46 +1,24 @@
-// class Solution {
-// public:
-//     vector<vector<int>> ans;
-    
-//     void sub(vector<int> &nums, int i, vector<int> temp)
-//     {
-//         if(i==nums.size())
-//         {
-//             ans.push_back(temp);
-//             return;
-//         }
-        
-//         sub(nums, i+1, temp);
-//         temp.push_back(nums[i]);
-//         sub(nums, i+1, temp);
-//     }
-    
-//     vector<vector<int>> subsets(vector<int>& nums) {
-//         vector<int> temp;       
-//         sub(nums, 0, temp); // or sub(nums, 0, vector<int> {});
-//         return ans;
-//     }
-// };
+
 
 class Solution
 {
     public:
-     vector<vector<int>> ans ;
-    void subsets(int index , vector<int> &arr , vector<int> ds){
+    
+    void subsets(int index , vector<vector<int>> &ans , vector<int> &arr , vector<int> ds){
         if(index == arr.size()){
             ans.push_back(ds) ;
             return ;
         }
-        subsets(index+1,arr,ds);
+        subsets(index+1,ans,arr,ds);
         ds.push_back(arr[index]) ;
-        subsets(index+1,arr,ds) ;
-        // ds.pop_back();
+        subsets(index+1,ans,arr,ds) ;
     }
     vector<vector<int> > subsets(vector<int>& A)
     {
         //code here
+        vector<vector<int>> ans ;
         vector<int> ds ;
-        subsets(0, A , ds) ;
+        subsets(0, ans , A , ds) ;
         return ans;
     }
 };
