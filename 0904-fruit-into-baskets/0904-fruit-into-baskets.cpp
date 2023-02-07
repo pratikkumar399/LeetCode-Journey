abc04ecf6818bd0feccfbaa10e841c39672f395a
Pratik Rai <pratikkumar399@gmail.com>
2023-02-08 01:12:12 +0530

    Time: 191 ms (59.27%), Space: 71.2 MB (48.12%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
   
-    static bool cmp(const pair<int, int> a , const pair<int, int> b){
-        return  a.second < b.second ;
-    }
-    
     int totalFruit(vector<int>& fruits) {
         int count = 0 ; 
-        map<int, int>  m ;
+        unordered_map<int, int>  m ;
         
        int start = 0 ;
        int end = 0 ;
        int maxi = INT_MIN ;
-        for(auto it : fruits){
+        while(end<fruits.size()){
            m[fruits[end]]++ ;
            end++ ;
             while(m.size() >  2){ 
               m[fruits[start]]-- ;
-            // we can remove that element from the map
-            if(m[fruits[start]] == 0){
+              if(m[fruits[start]] == 0){
               m.erase(fruits[start]) ; 
-             
             }
-           start++ ;
+            start++ ;
           }
-          maxi =max(maxi , end - start);
+           maxi =max(maxi , end - start);
         }
-        
-        
-      
         return maxi ;
     }
 };
