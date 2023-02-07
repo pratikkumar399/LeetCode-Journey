98d0d03556f85229d26552102a961ac59adb881b
Pratik Rai <pratikkumar399@gmail.com>
2023-02-08 00:21:23 +0530

    Time: 216 ms (31.44%), Space: 75.2 MB (13.09%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+  
+    static bool cmp(const pair<int, int> a , const pair<int, int> b){
+        return  a.second < b.second ;
+    }
+    
+    int totalFruit(vector<int>& fruits) {
+        int count = 0 ; 
+        map<int, int>  m ;
+        
+       int start = 0 ;
+       int end = 0 ;
+        for(auto it : fruits){
+           m[fruits[end]]++ ;
+           end++ ;
+            if(m.size() >  2){ 
+              m[fruits[start]]-- ;
+            // we can remove that element from the map
+            if(m[fruits[start]] == 0){
+              m.erase(fruits[start]) ; 
+             
+            }
+           start++ ;
+          }
+        }
+        
+        
+      
+        return end - start ;
+    }
+};
