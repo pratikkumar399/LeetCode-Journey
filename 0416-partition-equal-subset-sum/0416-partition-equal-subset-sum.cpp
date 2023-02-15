f96ce7a4b519946504a8edf9e04696827a354104
Pratik Rai <pratikkumar399@gmail.com>
2023-02-15 17:43:28 +0530

    Time: 286 ms (66.22%), Space: 95.1 MB (32.34%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool f(int index , int target , vector<int> &arr , vector<vector<int>> &dp){
-      if(target < 0 ) return false; 
-      if(target == 0) return true; 
-      if(index == 0) return (arr[0] == target ) ;
-
+    bool f(int index , vector<int> &nums , vector<vector<int>> &dp , int target){
+      if(target < 0) return false ;
+      if(target == 0) return true ;
+      if(index == 0) return (nums[0] == target) ;
+      
+     if(dp[index][target] != -1) return dp[index][target] ;
+     bool notTake = f(index - 1 , nums , dp , target) ;
+     bool take = f(index - 1 , nums , dp , target - nums[index]) ;
      
-      if(dp[index][target] != -1) return dp[index][target] ;
-      bool notTake = f(index - 1 , target , arr , dp);
-      bool take = f(index - 1 , target - arr[index] , arr , dp) ;
-
-      return dp[index][target] = take | notTake ;
-    } 
-
-    bool subsetSumToK(int n, int k, vector<int> &arr) {
-        // Write your code here.
-        vector<vector<int>> dp(n , vector<int> (k + 1 , -1)) ;
-        return f(n-1 , k , arr , dp) ;
-    }
+     return dp[index][target] = take or notTake ; 
+   }
+  
+   
+   bool subset(  int index ,int target , vector<int> &nums){
+    
+    vector<vector<int>> dp(index , vector<int>(target +1 , -1)) ;
+     
+     return f(index -1 , nums , dp , target) ;
+   }
   
     bool canPartition(vector<int>& nums) {
          int sum = 0 ;
@@ -25,6 +26,6 @@ public:
          
         if(sum % 2) return false ;
         int target = sum /2 ;
-        return subsetSumToK(nums.size() , target , nums) ;
+        return subset(nums.size() , target , nums) ;
     }
 };
